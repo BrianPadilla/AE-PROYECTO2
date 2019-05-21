275f3cc142f6da1f825b5309a06ef1dbeda935d4
BrianPadilla <supertro_lol@hotmail.com>
2019-05-20 22:30:09 -0600

    Creacion de Base de Datos en FireBase y acomodo de el entorno para su uso

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+
+    apiKey: "AIzaSyA-ra6COQjlg4fCzv5bbMw5zi5gyBJAetY",
+    authDomain: "todolist-de7e0.firebaseapp.com",
+    databaseURL: "https://todolist-de7e0.firebaseio.com",
+    projectId: "todolist-de7e0",
+    storageBucket: "todolist-de7e0.appspot.com",
+    messagingSenderId: "806682336633",
+    appId: "1:806682336633:web:569a446a1232b595"
+    
+  }
 };
 
 /*
